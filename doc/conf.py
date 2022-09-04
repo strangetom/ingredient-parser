689ec004c3c2f483bc6115f5e64ea0fd4976f6cc
Tom Strange <tpstrange@gmail.com>
2022-09-04 08:39:30 +0100

    Revert "Reorganise docs in an attempt to deploy to github pages"
    This reverts commit 8856a5eda8fe2ab453c307d617659b917814dcd4.

--- doc/conf.py
+++ doc/conf.py
@@ -9,34 +9,36 @@ import sys
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Ingredient Parser"
-copyright = "2022, Tom Strange"
-author = "Tom Strange"
-release = "0.1.0-alpha1"
+project = 'Ingredient Parser'
+copyright = '2022, Tom Strange'
+author = 'Tom Strange'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_design"]
-autodoc_typehints = "none"
+extensions = [
+  'sphinx.ext.autodoc', 
+  'sphinx.ext.napoleon',
+  'sphinx_design'
+]
+autodoc_typehints = 'none'
 
-templates_path = ["_templates"]
-exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # Modify path to make ingredient_parser package available for autodoc
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "pydata_sphinx_theme"
-html_static_path = ["_static"]
-html_css_files = ["custom.css", "pygments.css"]
-
-html_context = {"default_mode": "dark"}
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+html_css_files = ['custom.css', 'pygments.css']
 
 html_theme_options = {
-    "collapse_navigation": True,
-    "pygment_light_style": "gruvbox-light",
-    "pygment_dark_style": "gruvbox-dark",
+  "collapse_navigation": True,
+   "pygment_light_style": "gruvbox-light",
+   "pygment_dark_style": "gruvbox-dark"
 }
