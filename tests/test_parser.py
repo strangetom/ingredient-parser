4d4de157bae14498b449dceab7b92c667b0a978c
Tom Strange <tpstrange@gmail.com>
2023-04-05 16:51:53 +0100

    Tests: Add initial tests for sentences with ambiguous units, units after the ingredient name, comments before ingredient name

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -55,12 +55,11 @@ class TestParser:
         for key in true_labels:
             assert parsed[key] == true_labels[key]
 
-
-class TestRegexParser:
-    @pytest.mark.parametrize("data", load_test_cases("tests/test_parser_simple.json"))
-    def test_parser_simple(self, data):
-        """Test simple cases of ingredient sentences that the regex parser should have
-        no issues getting correct
+    @pytest.mark.parametrize(
+        "data", load_test_cases("tests/test_parser_alternate_unit_position.json")
+    )
+    def test_parser_alternate_unit_position(self, data):
+        """Test cases where the unit appears after the ingredient name
 
         Parameters
         ----------
@@ -71,9 +70,78 @@ class TestRegexParser:
         """
         sentence = data["sentence"]
         true_labels = data["labels"]
-        parsed = parse_ingredient_regex(sentence)
+        parsed = parse_ingredient(sentence)
 
         # parse_ingredient returns a dict with more keys than just the labels
         # so iterate through each label in true_labels and only check them.
         for key in true_labels:
             assert parsed[key] == true_labels[key]
+
+    @pytest.mark.parametrize(
+        "data", load_test_cases("tests/test_parser_ambiguous_unit.json")
+    )
+    def test_parser_ambiguous_unit(self, data):
+        """Test cases where the same word could be a unit or an ingredient,
+        depending on context e.g. clove
+
+        Parameters
+        ----------
+        data : Dict[str, Dict[str, str]]
+            Dictionary of sample data with the following keys
+                sentence - the input sentence
+                labels - the correct labels the parser should return
+        """
+        sentence = data["sentence"]
+        true_labels = data["labels"]
+        parsed = parse_ingredient(sentence)
+
+        # parse_ingredient returns a dict with more keys than just the labels
+        # so iterate through each label in true_labels and only check them.
+        for key in true_labels:
+            assert parsed[key] == true_labels[key]
+
+    @pytest.mark.parametrize(
+        "data", load_test_cases("tests/test_parser_comment_before_name.json")
+    )
+    def test_parser_comment_before_name(self, data):
+        """Test cases where the comment appears before the ingredient name,
+        e.g. 1 chopped carrot
+
+        Parameters
+        ----------
+        data : Dict[str, Dict[str, str]]
+            Dictionary of sample data with the following keys
+                sentence - the input sentence
+                labels - the correct labels the parser should return
+        """
+        sentence = data["sentence"]
+        true_labels = data["labels"]
+        parsed = parse_ingredient(sentence)
+
+        # parse_ingredient returns a dict with more keys than just the labels
+        # so iterate through each label in true_labels and only check them.
+        for key in true_labels:
+            assert parsed[key] == true_labels[key]
+
+
+#class TestRegexParser:
+#    @pytest.mark.parametrize("data", load_test_cases("tests/test_parser_simple.json"))
+#    def test_parser_simple(self, data):
+#        """Test simple cases of ingredient sentences that the regex parser should have
+#        no issues getting correct
+#
+#        Parameters
+#        ----------
+#        data : Dict[str, Dict[str, str]]
+#            Dictionary of sample data with the following keys
+#                sentence - the input sentence
+#                labels - the correct labels the parser should return
+#        """
+#        sentence = data["sentence"]
+#        true_labels = data["labels"]
+#        parsed = parse_ingredient_regex(sentence)
+#
+#        # parse_ingredient returns a dict with more keys than just the labels
+#        # so iterate through each label in true_labels and only check them.
+#        for key in true_labels:
+#            assert parsed[key] == true_labels[key]
