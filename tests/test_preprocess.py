75d939692f131989b58477c682916fb5b1279e5d
Tom Strange <tpstrange@gmail.com>
2023-07-18 19:36:11 +0100

    Tests: Add tests for new _remove_unit_trailing_period function

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -369,6 +369,35 @@ class TestPreProcessor_split_quantity_and_units:
         assert p._split_quantity_and_units(input_sentence) == '2.5" square chocolate'
 
 
+class TestPreProcessor_remove_unit_trailing_period:
+    def test_tsp(self, p):
+        """
+        "tsps." is replaced by "tsps"
+        """
+        input_sentence = "2 tsps. ground cinnamon"
+        assert p._remove_unit_trailing_period(input_sentence) == "2 tsps ground cinnamon"
+
+    def test_tbsp(self, p):
+        """
+        "tbsp." is replaced by "tbsp"
+        """
+        input_sentence = "1 tbsp. tomato sauce"
+        assert p._remove_unit_trailing_period(input_sentence) == "1 tbsp tomato sauce"
+
+    def test_lb(self, p):
+        """
+        "lbs." is replaced by "lbs"
+        """
+        input_sentence = "3 lbs. minced beef"
+        assert p._remove_unit_trailing_period(input_sentence) == "3 lbs minced beef"
+
+    def test_oz(self, p):
+        """
+        "oz." is replaced by "oz"
+        """
+        input_sentence = "1 12oz. can chopped tomatoes"
+        assert p._remove_unit_trailing_period(input_sentence) == "1 12oz can chopped tomatoes"
+
 class TestPreProcessor_singlarise_units:
     def test_embedded(self, p):
         """
