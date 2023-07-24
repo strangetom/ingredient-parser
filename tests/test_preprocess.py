0d076c640a26b1e5bddabc6bd8c0e4a17e0fa9a1
Tom Strange <tpstrange@gmail.com>
2023-07-24 18:19:11 +0100

    Test: Add tests for _replace_string_range

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -404,6 +404,51 @@ class TestPreProcessor_remove_unit_trailing_period:
         )
 
 
+class TestPreProcessor_replace_string_range:
+    def test_integers(self, p):
+        """
+        Test range with format <num> or <num> where <num> are integers
+        """
+        input_sentence = "4 9 or 10 inch flour tortillas"
+        assert p._replace_string_range(input_sentence) == "4 9-10 inch flour tortillas"
+
+    def test_decimals(self, p):
+        """
+        Test range with format <num> or <num> where <num> are decimals
+        """
+        input_sentence = "1 15.5 or 16 ounce can black beans"
+        assert (
+            p._replace_string_range(input_sentence) == "1 15.5-16 ounce can black beans"
+        )
+
+    def test_hyphens(self, p):
+        """
+        Test range where the numbers are followed by hyphens
+        """
+        input_sentence = "1 6- or 7-ounce can of wild salmon"
+        assert (
+            p._replace_string_range(input_sentence) == "1 6-7-ounce can of wild salmon"
+        )
+
+    def test_en_dash(self, p):
+        """
+        Test range where the numbers are followed by en-dashes
+        """
+        input_sentence = "1 6– or 7–ounce can of wild salmon"
+        assert (
+            p._replace_string_range(input_sentence) == "1 6-7–ounce can of wild salmon"
+        )
+
+    def test_en_dash(self, p):
+        """
+        Test range where the numbers are followed by em-dashes
+        """
+        input_sentence = "1 6— or 7—ounce can of wild salmon"
+        assert (
+            p._replace_string_range(input_sentence) == "1 6-7—ounce can of wild salmon"
+        )
+
+
 class TestPreProcessor_singlarise_units:
     def test_embedded(self, p):
         """
