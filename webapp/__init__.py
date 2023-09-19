21c4068ae24a70b772cbe37e6606f6239725b068
Tom Strange <tpstrange@gmail.com>
2023-09-19 20:14:23 +0100

    Feature: Support preparation information in webapp

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -36,6 +36,7 @@ def home():
         tokens=zip(parser_info.PostProcessor.tokens, parser_info.PostProcessor.labels),
         name=parsed.name if parsed.name is not None else IngredientText("", 0),
         amounts=parsed.amount,
+        preparation=parsed.preparation if parsed.preparation is not None else IngredientText("", 0),
         comment=parsed.comment if parsed.comment is not None else IngredientText("", 0),
         other=parsed.other if parsed.other is not None else IngredientText("", 0),
     )
