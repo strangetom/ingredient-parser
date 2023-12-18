605104508cb18822615f9cf9f6169e2442b17755
Tom Strange <tpstrange@gmail.com>
2023-12-18 17:23:39 +0000

    Tests: Update PostProcessor test to cover both cases where discard_isolated_stop_words is True and False.

--- tests/postprocess/test_postprocess.py
+++ tests/postprocess/test_postprocess.py
@@ -10,22 +10,48 @@ from ingredient_parser.postprocess import (
 
 @pytest.fixture
 def p():
-    """Define a PostProcessor object to use for testing the PostProcessor
-    class methods.
+    """Define a PostProcessor object with discard_isolated_stop_words set to True
+    to use for testing the PostProcessor class methods.
     """
-    sentence = "2 14 ounce cans coconut milk"
-    tokens = ["2", "14", "ounce", "can", "coconut", "milk"]
-    labels = ["QTY", "QTY", "UNIT", "UNIT", "NAME", "NAME"]
+    sentence = "2 14 ounce cans of coconut milk"
+    tokens = ["2", "14", "ounce", "can", "of", "coconut", "milk"]
+    labels = ["QTY", "QTY", "UNIT", "UNIT", "COMMENT", "NAME", "NAME"]
     scores = [
-        0.9991370577083561,
-        0.9725378063405858,
-        0.9978510889596651,
-        0.9922350007952175,
-        0.9886087821704076,
-        0.9969237827902526,
+        0.9995971493946465,
+        0.9941502269360797,
+        0.9978571790476597,
+        0.9343053167729019,
+        0.8352859914316577,
+        0.9907929042080257,
+        0.9954196827665529,
     ]
 
-    return PostProcessor(sentence, tokens, labels, scores)
+    return PostProcessor(
+        sentence, tokens, labels, scores, discard_isolated_stop_words=True
+    )
+
+
+@pytest.fixture
+def p_no_discard():
+    """Define a PostProcessor object with discard_isolated_stop_words set to False
+    to use for testing the PostProcessor class methods.
+    """
+    sentence = "2 14 ounce cans of coconut milk"
+    tokens = ["2", "14", "ounce", "can", "of", "coconut", "milk"]
+    labels = ["QTY", "QTY", "UNIT", "UNIT", "COMMENT", "NAME", "NAME"]
+    scores = [
+        0.9995971493946465,
+        0.9941502269360797,
+        0.9978571790476597,
+        0.9343053167729019,
+        0.8352859914316577,
+        0.9907929042080257,
+        0.9954196827665529,
+    ]
+
+    return PostProcessor(
+        sentence, tokens, labels, scores, discard_isolated_stop_words=False
+    )
 
 
 class TestPostProcessor__builtins__:
@@ -34,7 +60,7 @@ class TestPostProcessor__builtins__:
         Test PostProcessor __str__
         """
         truth = """Post-processed recipe ingredient sentence
-\t[('2', 'QTY'), ('14', 'QTY'), ('ounce', 'UNIT'), ('can', 'UNIT'), \
+\t[('2', 'QTY'), ('14', 'QTY'), ('ounce', 'UNIT'), ('can', 'UNIT'), ('of', 'COMMENT'), \
 ('coconut', 'NAME'), ('milk', 'NAME')]"""
         assert str(p) == truth
 
@@ -42,28 +68,29 @@ class TestPostProcessor__builtins__:
         """
         Test PostProessor __repr__
         """
-        assert repr(p) == 'PostProcessor("2 14 ounce cans coconut milk")'
+        assert repr(p) == 'PostProcessor("2 14 ounce cans of coconut milk")'
 
 
 class TestPostProcessor_parsed:
     def test(self, p):
         """
-        Test fixture returns expected ParsedIngredient object
+        Test fixture returns expected ParsedIngredient object, with the word "of"
+        discarded due to discard_isolated_stop_words being set to True.
         """
         expected = ParsedIngredient(
-            name=IngredientText(text="coconut milk", confidence=0.992766),
+            name=IngredientText(text="coconut milk", confidence=0.993106),
             amount=[
                 IngredientAmount(
                     quantity="2",
                     unit="cans",
-                    confidence=0.9956860292517868,
+                    confidence=0.9669512330837742,
                     APPROXIMATE=False,
                     SINGULAR=False,
                 ),
                 IngredientAmount(
                     quantity="14",
                     unit="ounces",
-                    confidence=0.9725378063405858,
+                    confidence=0.9941502269360797,
                     APPROXIMATE=False,
                     SINGULAR=True,
                 ),
@@ -71,6 +98,38 @@ class TestPostProcessor_parsed:
             preparation=None,
             comment=None,
             other=None,
-            sentence="2 14 ounce cans coconut milk",
+            sentence="2 14 ounce cans of coconut milk",
         )
+
         assert p.parsed() == expected
+
+    def test_no_discard_isolated_stop_words(self, p_no_discard):
+        """
+        Test fixture returns expected ParsedIngredient object, with the word "of"
+        kept due to discard_isolated_stop_words being set to False.
+        """
+        expected = ParsedIngredient(
+            name=IngredientText(text="coconut milk", confidence=0.993106),
+            amount=[
+                IngredientAmount(
+                    quantity="2",
+                    unit="cans",
+                    confidence=0.9669512330837742,
+                    APPROXIMATE=False,
+                    SINGULAR=False,
+                ),
+                IngredientAmount(
+                    quantity="14",
+                    unit="ounces",
+                    confidence=0.9941502269360797,
+                    APPROXIMATE=False,
+                    SINGULAR=True,
+                ),
+            ],
+            preparation=None,
+            comment=IngredientText(text="of", confidence=0.835286),
+            other=None,
+            sentence="2 14 ounce cans of coconut milk",
+        )
+
+        assert p_no_discard.parsed() == expected
