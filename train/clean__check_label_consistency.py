e1a767b273f30af8cf742fbc5f345a3c752845f6
Tom Strange <tpstrange@gmail.com>
2024-05-10 16:59:27 +0100

    Chore: Align tokeniser in consistency utility with tokeniser in ingredient_parser.en._util

--- train/clean__check_label_consistency.py
+++ train/clean__check_label_consistency.py
@@ -12,19 +12,23 @@ from sklearn.pipeline import Pipeline
 
 from .training_utils import load_datasets
 
-## Tokeniser from preprocess.py ##
 # Define regular expressions used by tokenizer.
 # Matches one or more whitespace characters
 WHITESPACE_TOKENISER = re.compile(r"\S+")
 # Matches and captures one of the following: ( ) [ ] { } , " / : ;
-PUNCTUATION_TOKENISER = re.compile(r"([\(\)\[\]\{\}\,\"/:;])")
+PUNCTUATION_TOKENISER = re.compile(r"([\(\)\[\]\{\}\,/:;])")
+# Matches and captures full stop at end of string
+# (?>!\.\w) is a negative lookbehind that prevents matches if the last full stop
+# is preceded by a a full stop then a word character.
+FULL_STOP_TOKENISER = re.compile(r"(?<!\.\w)(\.)$")
 
 
 def tokenize(sentence: str) -> list[str]:
     """Tokenise an ingredient sentence.
+
     The sentence is split on whitespace characters into a list of tokens.
     If any of these tokens contains of the punctuation marks captured by
-    PUNCTUATION_TOKENISER, these are then split and isolated as a seperate
+    PUNCTUATION_TOKENISER, these are then split and isolated as a separate
     token.
 
     The returned list of tokens has any empty tokens removed.
@@ -46,11 +50,22 @@ def tokenize(sentence: str) -> list[str]:
 
     >>> tokenize("1-2 mashed bananas: as ripe as possible")
     ["1-2", "mashed", "bananas", ":", "as", "ripe", "as", "possible"]
+
+    >>> tokenize("1.5 kg bananas, mashed")
+    ["1.5", "kg", "bananas", ",", "mashed"]
+
+    >>> tokenize("Freshly grated Parmesan cheese, for garnish.")
+    ["Freshly", "grated", "Parmesan", "cheese", ",", "for", "garnish", "."]
     """
     tokens = [
         PUNCTUATION_TOKENISER.split(tok)
         for tok in WHITESPACE_TOKENISER.findall(sentence)
     ]
+    flattened = [tok for tok in chain.from_iterable(tokens) if tok]
+
+    # Second pass to separate full stops from end of tokens
+    tokens = [FULL_STOP_TOKENISER.split(tok) for tok in flattened]
+
     return [tok for tok in chain.from_iterable(tokens) if tok]
 
 
