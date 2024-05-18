632d363a9de030697fe49c0b6821cc660909c37e
Tom Strange <tpstrange@gmail.com>
2024-05-18 13:09:52 +0100

    Refactor: Handling of PUNC tokens when postprocessing, to avoid including a PUNC token in the score calculation only to remove the actual token because it wouldn't be grammatical.

---
+++ tests/postprocess/test_remove_adjacent_duplicates.py
@@ -0,0 +1,39 @@
+import pytest
+
+from ingredient_parser.en import PostProcessor
+
+
+@pytest.fixture
+def p():
+    """Define a PostProcessor object to use for testing the PostProcessor
+    class methods.
+    """
+    sentence = "2 14 ounce cans coconut milk"
+    tokens = ["2", "14", "ounce", "can", "coconut", "milk"]
+    labels = ["QTY", "QTY", "UNIT", "UNIT", "NAME", "NAME"]
+    scores = [
+        0.9991370577083561,
+        0.9725378063405858,
+        0.9978510889596651,
+        0.9922350007952175,
+        0.9886087821704076,
+        0.9969237827902526,
+    ]
+
+    return PostProcessor(sentence, tokens, labels, scores)
+
+
+class TestPostProcessor_remove_adjacent_duplicates:
+    def test_adjacent_duplicate(self, p):
+        """
+        Test that index of second "finely" is not returned
+        """
+        input_list = ["finely", "finely", "chopped"]
+        assert p._remove_adjacent_duplicates(input_list) == [1, 2]
+
+    def test_non_adjacent_duplicate(self, p):
+        """
+        Test that index of non-adjacent duplicate is returned
+        """
+        input_list = ["finely", "chopped", "finely"]
+        assert p._remove_adjacent_duplicates(input_list) == [0, 1, 2]
