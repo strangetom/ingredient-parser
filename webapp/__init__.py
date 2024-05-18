6e7262215ee4ed02123810b725e9c0c8ca630027
Tom Strange <tpstrange@gmail.com>
2024-05-18 18:32:39 +0100

    Feature: Add functionality to webapp to enable/disable parse_ingredient options

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -19,20 +19,41 @@ def home():
         Rendered HTML template
     """
     sentence = request.args.get("sentence", None)
+    discard_isolated_stop_words = (
+        request.args.get("discard_isolated_stop_words", None) == "on"
+    )
+    guess_name_fallback = request.args.get("guess_name_fallback", None) == "on"
+    string_units = request.args.get("string_units", None) == "on"
+    imperial_units = request.args.get("imperial_units", None) == "on"
+
     if sentence is None:
         return render_template(
             "index.html.jinja",
             display=False,
             sentence="",
+            discard_isolated_stop_words=True,
+            guess_name_fallback=True,
+            string_units=False,
+            imperial_units=False,
         )
 
-    parser_info = inspect_parser(sentence)
+    parser_info = inspect_parser(
+        sentence,
+        discard_isolated_stop_words=discard_isolated_stop_words,
+        guess_name_fallback=guess_name_fallback,
+        string_units=string_units,
+        imperial_units=imperial_units,
+    )
     parsed = parser_info.PostProcessor.parsed
 
     return render_template(
         "index.html.jinja",
         display=True,
         sentence=sentence,
+        discard_isolated_stop_words=discard_isolated_stop_words,
+        guess_name_fallback=guess_name_fallback,
+        string_units=string_units,
+        imperial_units=imperial_units,
         tokens=zip(
             parser_info.PostProcessor.tokens,
             parser_info.PostProcessor.labels,
