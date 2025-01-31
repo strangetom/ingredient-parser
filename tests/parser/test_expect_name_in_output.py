065a87c972d26216a22c646f884ef895000d6446
Tom Strange <tpstrange@gmail.com>
2025-01-31 17:43:09 +0000

    Tests: Fix expect_name_in_output tests again.............

--- tests/parser/test_expect_name_in_output.py
+++ tests/parser/test_expect_name_in_output.py
@@ -6,7 +6,7 @@ class Test_expect_name_in_output:
         """
         Test that the return name is not None
         """
-        sentence = "A few thyme sprigs"
+        sentence = "4 Heath bars (1 1/8 ounces each)"
         parsed = parse_ingredient(sentence, expect_name_in_output=True)
         assert parsed.name != []
 
@@ -14,7 +14,7 @@ class Test_expect_name_in_output:
         """
         Test that the returned name is None
         """
-        sentence = "A few thyme sprigs"
+        sentence = "4 Heath bars (1 1/8 ounces each)"
         parsed = parse_ingredient(sentence, expect_name_in_output=False)
         assert parsed.name == []
 
