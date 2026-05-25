49772d64e28e887029bb339923deb1a68f830d3a
Tom Strange <tpstrange@gmail.com>
2026-05-25 08:06:11 +0100

    Tests: Mark tests for expect_name_in_output kwarg with "model_dependent" so they can be filtered.

--- tests/parser/test_expect_name_in_output.py
+++ tests/parser/test_expect_name_in_output.py
@@ -1,36 +1,42 @@
 from unittest.mock import MagicMock
 
+import pytest
+
 from ingredient_parser import parse_ingredient
 from ingredient_parser.en.parser import guess_ingredient_name
 
 
 class Test_expect_name_in_output:
+    @pytest.mark.model_dependent
     def test_enabled(self):
         """
         Test that the return name is not []
         """
-        sentence = "1 can (14 fluid ounces, or 425 ml) coconut milk"
+        sentence = "1 cup, plus 2 tablespoons olive oil"
         parsed = parse_ingredient(sentence, expect_name_in_output=True)
         assert parsed.name != []
 
+    @pytest.mark.model_dependent
     def test_disabled(self):
         """
         Test that the returned name is []
         """
-        sentence = "1 can (14 fluid ounces, or 425 ml) coconut milk"
+        sentence = "1 cup, plus 2 tablespoons olive oil"
         parsed = parse_ingredient(sentence, expect_name_in_output=False)
         assert parsed.name == []
 
+    @pytest.mark.model_dependent
     def test_disabled_name_not_separate(self):
         """
         Test that the returned name is [] when not separating names
         """
-        sentence = "1 can (14 fluid ounces, or 425 ml) coconut milk"
+        sentence = "1 cup, plus 2 tablespoons olive oil"
         parsed = parse_ingredient(
             sentence, expect_name_in_output=False, separate_names=False
         )
         assert parsed.name == []
 
+    @pytest.mark.model_dependent
     def test_enabled_but_no_name(self):
         """
         Test that the return name is None even though guess_name_fallback is enabled.
