0e40edb999fe8002bebd997c3746692c1171de6a
Tom Strange <tpstrange@gmail.com>
2026-07-06 19:03:09 +0100

    Bugfix: Capture exceptions during gridsearch and output information without aborting entire gridsearch.

--- train/gridsearch.py
+++ train/gridsearch.py
@@ -587,8 +587,14 @@ def grid_search(args: argparse.Namespace):
         futures = [executor.submit(train_model_grid_search, *a) for a in arguments]
         logger.info(f"Queued for separate runs against {len(args.algos)} algorithms")
         for idx, future in enumerate(cf.as_completed(futures)):
-            logger.info(f"{convert_num_ordinal(idx + 1)} algorithm completed")
-            eval_results.append(future.result())
+            if exception := future.exception():
+                logger.error(
+                    f"{convert_num_ordinal(idx + 1)} algorithm failed with exception:",
+                    exc_info=exception,
+                )
+            else:
+                logger.info(f"{convert_num_ordinal(idx + 1)} algorithm completed.")
+                eval_results.append(future.result())
 
     # Sort with highest sentence accuracy first, then highest token accuracy
     eval_results = sorted(
