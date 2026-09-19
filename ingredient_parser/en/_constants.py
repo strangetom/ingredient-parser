e668ee77ec30e725d6209fe7fc3971c5ea53c3b2
Tom Strange <tpstrange@gmail.com>
2026-09-19 14:36:14 +0100

    Feature: Add pound to list of ambiguous units (i.e. "pound cake")

--- ingredient_parser/en/_constants.py
+++ ingredient_parser/en/_constants.py
@@ -140,6 +140,7 @@ AMBIGUOUS_UNITS = [
     "gram",  # e.g. gram (chickpea) flour
     "glass",  # e.g. glass noodles
     "stem",  # e.g. stem ginger
+    "pound",  # e.g. pound cake
 ]
 # Extend list automatically to include singular and capitalized forms
 _ambiguous_units_alt_forms = []
